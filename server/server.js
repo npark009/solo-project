c4e2141d464e2e43a633cf3d11ec97401705bc72
Nicholas Park <npark009@gmail.com>
2023-02-01 22:07:11 -0800

    All server requests completed

--- server/server.js
+++ server/server.js
@@ -11,67 +11,105 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Connected to MongoDB');
 });
+
 app.use(express.json());
 app.use(cors());
 app.use('/', express.static(path.join(__dirname, '../dist')));
 
 const Recommendations = require('./models/Recommendations');
 
-app.get('/location/:locationId', async (req, res) => {
-  const locations = await Recommendations.find();
+app.get('/location/:id', async (req, res) => {
+  const locations = await Recommendations.find({ _id: req.params.id });
   res.json(locations);
+});
+
+app.get('/locations', async (req, res) => {
+  const locations = await Recommendations.find({});
+  return res.send(locations);
 });
 
 app.post('/location/new', (req, res) => {
   const location = new Recommendations({
     location: req.body.location,
   });
+  console.log('req.body', req.body);
   location.save((err) => {
-    if (err) res.status(500).send(err);
-    else res.send(location);
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(location);
+    }
   });
 });
 
-app.put('/location/:locationId/restaurant/new', (req, res) => {
-  Recommendations.findOneAndUpdate(
-    { location: req.params.locationId },
-    (err, rec) => {
-      if (err) {
-        return res.status(500).send(err);
-      } else {
-        rec.restaurants.push({
-          name: req.body.restaurants.name,
-          cuisine: req.body.restaurants.cuisine,
-          rating: req.body.restaurants.rating,
-        });
-      }
-      rec.save((err, updatedRec) => {
-        if (err) return res.status(500).send(err);
-        else res.send(updatedRec);
-      });
-    }
-  );
+app.put('/location/:id/restaurant/new', async (req, res) => {
+  const rec = await Recommendations.findById(req.params.id);
+  console.log('reqbody', req.body);
+  console.log('1rec.restaurants', rec.restaurants);
+  if (rec) {
+    console.log('2rec.restaurants', rec.restaurants);
+    rec.restaurants.push({
+      name: req.body.name,
+      cuisine: req.body.cuisine,
+      rating: req.body.rating,
+    });
+    const updatedRec = await rec.save();
+    return res.send(updatedRec);
+  }
+  res.status(500).send({ error: 'Could not update the recommendation' });
 });
 
-app.put('/location/:locationId/bar/new', (req, res) => {
-  Recommendations.findOneAndUpdate(
-    { location: req.params.locationId },
-    (err, rec) => {
-      if (err) {
-        return res.status(500).send(err);
-      } else {
-        rec.bars.push({
-          name: req.body.bars.name,
-          type: req.body.bars.type,
-          rating: req.body.bars.rating,
-        });
-      }
-      rec.save((err, updatedRec) => {
-        if (err) return res.status(500).send(err);
-        else res.send(updatedRec);
-      });
-    }
-  );
+app.put('/location/:id/bar/new', async (req, res) => {
+  const rec = await Recommendations.findById(req.params.id);
+  if (rec) {
+    rec.bars.push({
+      name: req.body.name,
+      type: req.body.type,
+      rating: req.body.rating,
+    });
+    const updatedRec = await rec.save();
+    return res.send(updatedRec);
+  }
+  res.status(500).send({ error: 'Could not update the recommendation' });
+});
+
+app.delete('/location/:id', async (req, res) => {
+  //delete lcoation as a whole
+  const result = await Recommendations.findByIdAndDelete(req.params.id);
+  if (result) return res.send(result);
+  res.status(500).send({ error: 'Could not delete the location' });
+});
+
+app.delete('/location/:id/restaurant/:name', async (req, res) => {
+  //delete A particular restaurant recommendation
+  const rec = await Recommendations.findById(req.params.id);
+  if (rec) {
+    rec.restaurants = rec.restaurants.filter((restaurant) => {
+      return (
+        restaurant.name.toLowerCase().split(' ').join('') !==
+        decodeURIComponent(req.params.name.toLowerCase())
+      );
+    });
+    const updatedRec = await rec.save();
+    return res.send(updatedRec);
+  }
+  res.status(500).send({ error: 'Could not delete the restaurant' });
+});
+
+app.delete('/location/:id/bar/:name', async (req, res) => {
+  //delete A particular restaurant recommendation
+  const rec = await Recommendations.findById(req.params.id);
+  if (rec) {
+    rec.bars = rec.bars.filter((bar) => {
+      return (
+        bar.name.toLowerCase().split(' ').join('') !==
+        decodeURIComponent(req.params.name.toLowerCase())
+      );
+    });
+    const updatedRec = await rec.save();
+    return res.send(updatedRec);
+  }
+  res.status(500).send({ error: 'Could not delete the restaurant' });
 });
 
 app.use((err, req, res, next) => {
